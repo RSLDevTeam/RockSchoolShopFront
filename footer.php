755cee31aa6e791cd47de301e1f37a26b727cb89
Sarah Sajjad <sarahsajjad93@gmail.com>
2025-03-12 21:04:16 +0000

    google APi integration

--- footer.php
+++ footer.php
@@ -207,5 +207,15 @@
 
 <?php wp_footer(); ?>
 
+<script>
+function initAutocomplete() {
+    var input = document.getElementById('place-search');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    initAutocomplete();
+});
+</script>
 </body>
 </html>
