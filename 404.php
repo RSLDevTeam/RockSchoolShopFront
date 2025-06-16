39e7f1aaab97dd658750d4ee215bf8afd859f1a9
Sarah Sajjad <sarahsajjad93@gmail.com>
2025-06-16 12:34:29 +0100

    404 Page design

--- 404.php
+++ 404.php
@@ -13,45 +13,32 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'rockschool' ); ?></h1>
-			</header><!-- .page-header -->
+			<div class="container mx-auto p-2.5 max-w-[1440px] z-1">
+				<div class="tailwind-cols grid grid-cols-1 lg:grid-cols-3 pt-[1.5em] pb-[2em] gap-8 w-[85%] mx-auto px-4">
+					<div class="profile-col-1 col-span-1">
+						<div class="mb-6">
+							<div class="provider-photo aos-init aos-animate" data-aos="zoom-in">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/guitar-404.png" alt="">
+							</div>
+						</div>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'rockschool' ); ?></p>
+					</div>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'rockschool' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$rockschool_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'rockschool' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$rockschool_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
+					<div class="profile-col-2 col-span-1 lg:col-span-2">
+						<div class="prose max-w-none">
+							<header class="provider-header">
+								<h1 data-aos="zoom-in" class="aos-init aos-animate"> Oops! Looks like the tune’s gone missing. </h1>
+							</header>
+							<div class="provider-biog aos-init aos-animate" data-aos="zoom-in">
+								<p> You’ve hit a flat note! 
+									<br/> Don’t worry, let’s get you back in key. </p>
+							</div>
+							<div class="w-full flex justify-center items-center mt-8 aos-init aos-animate" data-aos="zoom-in">
+								<a href="<?php echo esc_url(home_url('/')); ?>"> <button> Go to Home </button> </a>
+							</div>
+					</div>
+				</div>
+			</div>
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
