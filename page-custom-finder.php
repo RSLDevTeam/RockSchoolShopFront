186efcf418f958082f7449baf9fc43d89b80c659
Sarah Sajjad <sarahsajjad93@gmail.com>
2026-08-19 12:21:02 +0100

    Update marker icons in custom finder map

--- page-custom-finder.php
+++ page-custom-finder.php
@@ -103,9 +103,7 @@ $distance = isset($_GET['distance']) ? sanitize_text_field($_GET['distance']) : 
 						<div
 							id="finder-map"
 							class="w-full h-[600px] shadow-md"
-							data-marker-provider="<?php echo esc_url(rsl_shopfront_get_provider_marker_url('provider')); ?>"
-							data-marker-associate="<?php echo esc_url(rsl_shopfront_get_provider_marker_url('associate')); ?>"
-							data-marker-franchise="<?php echo esc_url(rsl_shopfront_get_provider_marker_url('franchise')); ?>"
+							data-marker-icon="<?php echo get_template_directory_uri(); ?>/img/map-marker-single.svg"
 							data-cluster-icon="<?php echo get_template_directory_uri(); ?>/img/map-marker-single.svg">
 						</div>
 					</div>
